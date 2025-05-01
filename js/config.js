e545635af3f4283d9bab87f69073e8d86b8aeb42
JPN0912 <prateeknaidu96@gmail.com>
2025-05-01 16:02:38 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_jaPUhXUNF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1uc1k75eljnvbji7ebu1idcu7h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '69fc1nlgp8ma8hf0nvn60n7kbh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
